db75036d09d453c5f2f31e1c7f4a542cf0153ae5
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-01-31 14:30:25 +0530

    fetching db credentials from config file

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 const config = require("./config.js");
 
 module.exports = mysql.createConnection({
-  host: "remotemysql.com",
-  database: "HuQ03BCoYI",
-  user: "HuQ03BCoYI",
-  password: "ZqFx1BmpV6",
+  host: config.MYSQL_HOST,
+  database: config.MYSQL_DATABASE,
+  user: config.MYSQL_USER,
+  password: config.MYSQL_PASSWORD,
 });
