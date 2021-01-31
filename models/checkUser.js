1c60a6a2d83b39befaf4b7518cd5ab9c6367ae72
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-01-31 13:53:25 +0530

    Added method to find by id

--- models/checkUser.js
+++ models/checkUser.js
@@ -2,7 +2,6 @@ const database = require("../config/dbConnect");
 
 database.connect((err) => {
   if (err) {
-    // res.send("Internal server error");
     console.log(err);
   }
 });
@@ -13,7 +12,18 @@ exports.userPresent = (email) => {
       `select Email from Users where Email = "${email}"`,
       (error, results, fields) => {
         if (error) return reject(error);
-        console.log(results);
+        resolve(results);
+      }
+    );
+  });
+};
+
+exports.userIdPresent = (id) => {
+  return new Promise((resolve, reject) => {
+    database.query(
+      `select Gid from Users where Gid = "${id}"`,
+      (error, results, fields) => {
+        if (error) return reject(error);
         resolve(results);
       }
     );
@@ -26,7 +36,30 @@ exports.getUser = (email) => {
       `select * from Users where Email = "${email}"`,
       (error, results, fields) => {
         if (error) return reject(error);
-        console.log(results);
+        resolve(results);
+      }
+    );
+  });
+};
+
+exports.getUserByGid = (id) => {
+  return new Promise((resolve, reject) => {
+    database.query(
+      `select * from Users where Gid = "${id}"`,
+      (error, results, fields) => {
+        if (error) return reject(error);
+        resolve(results);
+      }
+    );
+  });
+};
+
+exports.getUserById = (id) => {
+  return new Promise((resolve, reject) => {
+    database.query(
+      `select * from Users where id = "${id}"`,
+      (error, results, fields) => {
+        if (error) return reject(error);
         resolve(results);
       }
     );
