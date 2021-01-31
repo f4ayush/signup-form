239d7d1570f5cf9983a3a037e4a97a311238bb94
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-01-31 13:52:41 +0530

    Added google startegy

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,10 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
-
+const GoogleStrategy = require("passport-google-oauth20");
 // Load User model
-// const User = require("../models/User");
 const checkUser = require("../models/checkUser");
 
+const addUser = require("../models/addUser");
 module.exports = function (passport) {
   passport.use(
     new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
@@ -14,7 +14,6 @@ module.exports = function (passport) {
           return done(null, false, { message: "That email is not registered" });
         }
 
-        // bcrypt.compare(password);
         // Match password
         bcrypt.compare(password, data[0].Password, (err, isMatch) => {
           if (err) throw err;
@@ -28,20 +27,53 @@ module.exports = function (passport) {
     })
   );
 
+  // google signin
+  passport.use(
+    new GoogleStrategy(
+      {
+        // options for google strategy
+        callbackURL: "/redirect",
+        clientID:
+          "226612889594-70361oos5r8n8lfm23ahqaado7avisni.apps.googleusercontent.com",
+        clientSecret: "GrB-xRyQk6zEkq-p3BY8EZtL",
+      },
+      (accessToken, refreshToken, profile, done) => {
+        checkUser.userIdPresent(profile.id).then((data) => {
+          if (data[0] == undefined) {
+            console.log("a");
+            addUser
+              .addById(profile.id, profile.displayName)
+              .then(console.log)
+              .catch(console.error);
+            checkUser
+              .getUserByGid(profile.id)
+              .then((data) => done(null, data[0]))
+              .catch(console.error);
+          } else {
+            console.log("b");
+            checkUser
+              .getUserByGid(profile.id)
+              .then((data) => done(null, data[0]))
+              .catch(console.error);
+          }
+        });
+      }
+    )
+  );
+
   passport.serializeUser(function (user, done) {
-    done(null, user.Email);
+    console.log(user.id);
+    done(null, user.id);
   });
 
   passport.deserializeUser(function (email, done) {
-    // console.log(id);
     checkUser
-      .getUser(email)
+      .getUserById(email)
       .then((data) => {
-        console.log(data[0].Name);
         done(null, data[0].Name);
       })
-      .catch(() => {
-        console.log("err");
+      .catch((err) => {
+        console.log(err);
       });
   });
 };
