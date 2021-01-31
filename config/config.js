656fcd26f1396d88020480f2eaef0c609999e0f0
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-01-31 14:29:25 +0530

    corrected variables name and Added client_id and client_secret

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MYSQL_HOST: process.env.HOST,
-  MYSQL_DATABASE: process.env.DATABASE,
-  MYSQL_USER: process.env.USER,
-  MYSQL_PASSWORD: process.env.PASSWORD,
+  MYSQL_HOST: process.env.MYSQL_HOST,
+  MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+  MYSQL_USER: process.env.MYSQL_USER,
+  MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+  CLIENT_ID = process.env.CLIENT_ID,
+  CLIENT_SECRET = process.env.CLIENT_SECRET
 };
+
+console.log(process.env);
