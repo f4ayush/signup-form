030591b69cfe76cc0915b0ad52d2f20972c32d29
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-01-31 13:54:17 +0530

    removed syntax error in query

--- models/addUser.js
+++ models/addUser.js
@@ -11,3 +11,15 @@ exports.add = (email, name, password) => {
     );
   });
 };
+
+exports.addById = (id, name) => {
+  return new Promise((resolve, reject) => {
+    database.query(
+      `insert into Users(Gid, Name) values("${id}", "${name}")`,
+      (error, results, fields) => {
+        if (error) return reject(error);
+        resolve(results);
+      }
+    );
+  });
+};
